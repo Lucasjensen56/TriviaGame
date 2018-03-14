f0db1b40a08dc246f5b52ee972f3aebb4d533b9d
Lucas Jensen <lucasjensen56@gmail.com>
2018-03-14 17:23:54 -0500

    added questions array

---
+++ assets/javascript/app.js
@@ -0,0 +1,70 @@
+
+var QuestionsAndAnswers = [
+
+	{
+		question: "What does Dumbledore find in the Room of Requriement?",
+		answers: {
+			a: "chamber pots",
+			b: "telescopes",
+			c: "feather dusters",
+			d: "pyjamas"
+		},
+		correctAnswer: "a"
+	},
+
+	{
+		question: "Who isn't a Hogwarts House Ghost?",
+		answers: {
+			a: "Nearly Headless Nick",
+			b: "Peeves",
+			c: "The Grey Lady",
+			d: "The bloody Baron"
+		}
+		correctAnswer: "b"
+	},
+
+	{
+		question: "Which Defense Against the Dark Arts teacher filled his office with pictures of himself?"
+		answers: {
+			a: "Quirrel",
+			b: "Lupin",
+			c: "Lockhart",
+			d: "Moody"
+		}
+		correctAnswer: "c"
+	},
+
+	{
+		question: "How do first year students get to Hogwarts after exiting the train?"
+		answers: {
+			a: "Hippogriffs",
+			b: "Broomsticks",
+			c: "side-Along-Apparition",
+			d: "Magical boats across the lake"
+		}
+		correctAnswer: "d"
+	}
+];
+
+
+
+
+
+// startGame
+
+// Player is display with screen with button. 
+// On this page it says"click to start TriviaGame"
+
+// When the button is clicked start game, start timer and display first set up trivia games
+
+// Random trivia question is generated with 4 answers
+	// if the player chooses correctly they win++
+		 	// win is counted and displayed at the end of the game
+	// else if the playe choose the incorrect answer lose++
+			// lose count is stored and displayled at the end of the game
+
+// After the time runs out the next question is displayed
+
+// After player has cycled through all questions display how many times they won and lost
+
+
